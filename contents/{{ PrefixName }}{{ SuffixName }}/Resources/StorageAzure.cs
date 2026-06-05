5ee2b37a46669560bc0b0c95b83979022f4f33ac
Jimmie Fulton <jimmie@ybor.ai>
2026-06-05 12:36:29 -0700

    feat: dotnet-resource-azure-blob-library archetype content (initial import for review)

---
+++ contents/{{ PrefixName }}{{ SuffixName }}/Resources/StorageAzure.cs
@@ -0,0 +1,16 @@
+using Azure.Storage.Blobs;
+using Azure.Storage;
+
+namespace {{ PrefixName }}{{ SuffixName }}.Resources;
+
+public static class StorageAzureExtensions
+{
+    public static IServiceCollection AddStorageAzure(this IServiceCollection services, Settings settings)
+    {
+        var credential = new StorageSharedKeyCredential(
+            settings.AzureAccountName, settings.AzureAccountKey);
+        var client = new BlobServiceClient(new Uri(settings.AzureEndpoint), credential);
+        services.AddSingleton(client);
+        return services;
+    }
+}
